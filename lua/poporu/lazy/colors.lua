b7b58f02173f489e23596387293b279c750f27f6
Boris Komarov <boris.komarov@proton.me>
2025-06-07 17:21:55 +0200

    lsp support removed - lol read docs
    lsp is making me too lazy, if needed use vscodium

--- lua/poporu/lazy/colors.lua
+++ lua/poporu/lazy/colors.lua
@@ -3,6 +3,7 @@ function ColorMyNvim(color)
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
--- lua/poporu/set.lua
+++ lua/poporu/set.lua
@@ -1,11 +1,9 @@
-vim.opt.guicursor = ""
-
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.opt.cursorline = true
 
 vim.opt.hlsearch = false
@@ -13,11 +11,10 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+vim.opt.list = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
-
---vim.cmd("colorscheme retrobox")
