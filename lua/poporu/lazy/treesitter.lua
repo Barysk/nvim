b1afe3c5ad9d754a92bf02062444116722614417
Boris Komarov <boris.komarov@proton.me>
2025-06-07 21:45:55 +0200

    Based thing added

--- lua/poporu/lazy/treesitter.lua
+++ lua/poporu/lazy/treesitter.lua
@@ -6,6 +6,7 @@ return {
     opts = {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
         ensure_installed = {
+	    "odin",
             "rust",
             "c",
             "cpp",
