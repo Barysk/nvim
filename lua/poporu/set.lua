0a013b8a7712b9008c63d01eb72f67a6fbebf812
Boris Komarov <boris.komarov@proton.me>
2025-06-08 15:16:41 +0200

    not sure what, but in case something has been updated

--- lua/poporu/set.lua
+++ lua/poporu/set.lua
@@ -1,12 +1,12 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 8
+vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.cursorline = true
 
-vim.opt.hlsearch = false
+-- vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
