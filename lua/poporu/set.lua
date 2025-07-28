6cb040ef1701fb791a98a895cb83d61c4c318a76
Boris Komarov <boris.komarov@proton.me>
2025-07-28 17:10:25 +0200

    minor update

--- lua/poporu/set.lua
+++ lua/poporu/set.lua
@@ -5,7 +5,7 @@ vim.opt.cursorline = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.opt.smartindent = true
 
@@ -27,7 +27,8 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "80,100,120"
+
 
 vim.opt.list = true
 vim.opt.listchars = {
