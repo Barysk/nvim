5bb560f863194343ff6664bdf557eea29ecb464a
Boris <boris.komarov@proton.me>
2025-10-06 16:29:37 +0200

    added remove highlight binding

--- lua/poporu/remap.lua
+++ lua/poporu/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pp", ":e#<CR>")
+vim.keymap.set("n", "<leader>ii", ":nohl<CR>")
 
 -- paste without copying
 vim.keymap.set("x", "<leader>p", [["_dP]])
